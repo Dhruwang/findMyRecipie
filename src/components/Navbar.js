5819b165f637c8c35a86772401381322278b0816
Dhruwang <dhruwangjariwala18@gmail.com>
2022-11-25 23:46:46 +0530

    made minor changes in navbar and quotes alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-white py-4 px-4 fixed-top" >
+            <nav className="navbar navbar-expand-lg bg-white px-4 fixed-top" >
                 <div className="container-fluid">
                     <a className="navbar-brand fs-2" href="/">FindMyRecepie</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Quote() {
   return (
     <div className='w-50 text-center mx-auto'>
-                <p className=''>"One of the very best things about life is the way we must regularly stop whatever it is we are doing and devote our attention to eating. "</p>
+                <p >"One of the very best things about life is the way we must regularly stop whatever it is we are doing and devote our attention to eating. "</p>
                 <p className='fw-bold'>Luciano Pavarotti</p>
             </div>
   )
